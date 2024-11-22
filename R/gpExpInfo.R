df1c59c2581dc8143184326fcd2fc0055bdfcc14
„juliazervakos” <julia@zervakos.info>
2024-11-22 11:10:24 +0000

    Deleting the functions calculating gev expected information matrix entries, deleting lines with eps. deleting the constraint of xi > -0.5 for the gp expected info

--- R/gpExpInfo.R
+++ R/gpExpInfo.R
@@ -5,14 +5,10 @@
 #' @param scale,shape Numeric vectors. Respective values of the GP parameters
 #'   scale parameter \eqn{\sigma} and shape parameter \eqn{\xi}. For
 #'   `gpExpInfo`, `scale` and `shape` must have length 1.
-#' @param eps A numeric scalar. For values of \eqn{\xi} in `shape` that lie in
-#'   `(-eps, eps)` an approximation is used instead of a direct calculation.
-#'   See **Details**. If `eps` is a vector then only the first element is used.
 #' @details `gpExpInfo` calculates, for single pair of values
 #'   \eqn{(\sigma, \xi) = } `(scale, shape)`, the expected information matrix for a
 #'   single observation from a GP distribution with distribution function
 #'   \deqn{F(x) = P(X \leq x) = 1 - \left( 1 + \frac{\xi x}{\sigma} \right)^{-1/\xi}}
-#'   The GP expected information is defined only for \eqn{\xi > -0.5}
 #'
 #'   The other functions are vectorized and calculate the individual
 #'   contributions to the expected information matrix. For example, `gev11e`
@@ -38,102 +34,17 @@
 #'   `eps`.
 #' @examples
 #' # Expected information matrices for ...
-#' # ... scale = 2 and shape = -0.4
-#' gevExpInfo(2, -0.4)
-#' # ... scale = 3 and shape = 0.001
-#' gevExpInfo(3, 0.001)
-#' # ... scale = 3 and shape = 0
-#' gevExpInfo(3, 0)
 #' # ... scale = 1 and shape = 0.1
-#' gevExpInfo(1, 0.1)
-#'
-#' # The individual components of the latter matrix
-#' gev11e(1, 0.1)
-#' gev12e(1, 0.1)
-#' gev13e(1, 0.1)
-#' gev22e(1, 0.1)
-#' gev23e(1, 0.1)
-#' gev33e(0.1)
-#' @name gevExpInfo
-NULL
-## NULL
-
-#' @rdname gevExpInfo
+#' gpExpInfo(1, 0.1)
 #' @export
-gev11e <- function(scale, shape) {
-  m <- max(length(scale), length(shape))
-  return(rep_len(pxi(shape), m) / rep_len(scale ^ 2, m))
-}
-
-#' @rdname gevExpInfo
-#' @export
-gev22e <- function(scale, shape, eps = 3e-3) {
-  eps <- eps[1]
-  val <- gevExpInfoComp(fun = gev22eFn, fun0 = gev22e0Constant, xi = shape,
-                        eps = eps)
-  m <- max(length(scale), length(shape))
-  return(rep_len(val, m) / rep_len(scale ^ 2, m))
-}
-
-#' @rdname gevExpInfo
-#' @export
-gev33e <- function(shape, eps = 3e-3) {
-  eps <- eps[1]
-  val <- gevExpInfoComp(fun = gev33eFn, fun0 = gev33e0Constant, xi = shape,
-                        eps = eps)
-  return(val)
-}
-
-#' @rdname gevExpInfo
-#' @export
-gev12e <- function(scale, shape, eps = 3e-3) {
-  eps <- eps[1]
-  val <- gevExpInfoComp(fun = gev12eFn, fun0 = gev12e0Constant, xi = shape,
-                        eps = eps)
-  m <- max(length(scale), length(shape))
-  return(rep_len(val, m) / rep_len(scale ^ 2, m))
-}
-
-#' @rdname gevExpInfo
-#' @export
-gev13e <- function(scale, shape, eps = 3e-3) {
-  eps <- eps[1]
-  val <- gevExpInfoComp(fun = gev13eFn, fun0 = gev13e0Constant, xi = shape,
-                        eps = eps)
-  m <- max(length(scale), length(shape))
-  return(rep_len(val, m) / rep_len(scale, m))
-}
-
-#' @rdname gevExpInfo
-#' @export
-gev23e <- function(scale, shape, eps = 3e-3) {
-  eps <- eps[1]
-  val <- gevExpInfoComp(fun = gev23eFn, fun0 = gev23e0Constant, xi = shape,
-                        eps = eps)
-  m <- max(length(scale), length(shape))
-  return(rep_len(val, m) / rep_len(scale, m))
-}
-
-#' @rdname gevExpInfo
-#' @export
-gevExpInfo <- function(scale, shape, eps = 3e-3) {
+gpExpInfo <- function(scale, shape) {
   if (shape <= -0.5) {
-    stop("The GEV expected information is undefined for shape <= -0.5")
+    stop("The GP expected information is undefined for shape <= -0.5")
   }
-  # The expected information does not depend on loc
-  val <- matrix(NA, 3, 3)
-  val[1, 1] <- gev11e(scale, shape)
-  val[2, 2] <- gev22e(scale, shape, eps)
-  val[3, 3] <- gev33e(shape, eps)
-  val[2, 1] <- val[1, 2] <- gev12e(scale, shape, eps)
-  val[3, 1] <- val[1, 3] <- gev13e(scale, shape, eps)
-  val[3, 2] <- val[2, 3] <- gev23e(scale, shape, eps)
-  dimnames(val) <- list(c("loc", "scale", "shape"), c("loc", "scale", "shape"))
+  val <- matrix(NA, 2, 2)
+  val[1, 1] <- 1 / ((1 + 2 * shape) * scale ^ 2)
+  val[2, 2] <- 2 / ((1 + shape) * (1 + 2 * shape))
+  val[2, 1] <- val[1, 2] <- 1 / (scale * (1 + shape) * (1 + 2 * shape))
+  dimnames(val) <- list(c("scale", "shape"), c("scale", "shape"))
   return(val)
 }
-
-##
-
-val[1,1] <- 1/((1+2*shape)*scale^2)
-val[2,2] <- 2/((1+shape)*(1+2*shape))
-val[2,1] <- val[1,2] <- 1/(scale*(1+shape)*(1+2*shape))
