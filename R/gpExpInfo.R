63734bfad2b3f1d5d3d67ff0bd45faaf6ec23f9f
„juliazervakos” <julia@zervakos.info>
2024-11-28 12:32:04 +0000

    making fractions bigger using \displaystyle. condition for shape is > -0.5

--- R/gpExpInfo.R
+++ R/gpExpInfo.R
@@ -8,14 +8,15 @@
 #' @details `gpExpInfo` calculates, for a single pair of values
 #'   \eqn{(\sigma, \xi) = } `(scale, shape)`, the expected information matrix for a
 #'   single observation from a GP distribution with distribution function
-#'   \deqn{F(x) = P(X \leq x) = 1 - \left[ 1 + \frac{\xi x}{\sigma} \right]_+^{-1/\xi}}
-#'   The GP expected information is defined only for \eqn{\xi > -0.5}
+#'   \deqn{F(x) = P(X \leq x) = 1 - \left[ 1 + \displaystyle\frac{\xi x}{\sigma} \right]_+^{-1/\xi}}
+#'   where \eqn{x_+ = \max(x, 0)}.
+#'   The GP expected information is defined only for \eqn{\xi > -0.5}.
 #'
 #'   The matrix is of the form
-#'   \deqn{\mathbf{I} = \frac{1}{\sigma (1 + 2 \xi)} \begin{bmatrix}
-#'   \frac{1}{\sigma} & \frac{1}{1 + \xi} \\
-#'   \frac{1}{1 + \xi} & \frac{2}{1 + \xi}
-#'   \end{bmatrix}}
+#'   \deqn{\mathbf{I} = \displaystyle\frac{1}{\sigma (1 + 2 \xi)} \begin{bmatrix}
+#'   \displaystyle\frac{1}{\sigma} & \displaystyle\frac{1}{1 + \xi} \\
+#'   \displaystyle\frac{1}{1 + \xi} & \displaystyle\frac{2}{1 + \xi}
+#'   \end{bmatrix}.}
 #'
 #' @returns `gpExpInfo` returns a 2 by 2 numeric matrix with row and column
 #'   named `scale, shape`.
@@ -27,7 +28,7 @@
 #' @export
 gpExpInfo <- function(scale, shape) {
   if (shape <= -0.5) {
-    stop("The GP expected information is undefined for shape <= -0.5")
+    stop("The GP expected information is undefined for shape > -0.5")
   }
   val <- matrix(NA, 2, 2)
   val[1, 1] <- 1 / ((1 + 2 * shape) * scale ^ 2)
