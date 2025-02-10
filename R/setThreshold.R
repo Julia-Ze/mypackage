4d06d075ce26997e28f308e2377f964452c9f114
„juliazervakos” <julia@zervakos.info>
2025-02-10 12:05:05 +0000

    creating documentation for the setThreshold function

--- R/setThreshold.R
+++ R/setThreshold.R
@@ -1,15 +1,46 @@
-setThreshold <- function(formula, data, tau, mstop = 1000) {
+#' Fit a flexible (?) threshold ...?
+#'
+#' Fits a flexible threshold to the provided data to extract exceedences
+#' for further modelling. mention tau? using the function
+#' [`mboost::mboost()`][`mboost::mboost`].
+#'
+#' @inheritParams mboost::mboost
+#'
+#' @param tau bla bla
+#'
+#' @param mstop bla bla
+#'
+#' @return Returns a vector of fitted values which represent the threshold
+#' for a given value of the covariate
+#'
+#' @details See [`mboost::mboost()`][`mboost::mboost`] for information about
+#' the model and the fitting algorithm.
+#'
+#' @seealso what to put here?
+#'
+#' @examples
+#'
+#' set.seed(512024)
+#' n <- 100
+#' x <- stats::runif(n)
+#' sigma <- 1 + 2 * x
+#' xi <- 0.1
+#' y <- nieve::rGPD2(n = 1, scale = sigma, shape = xi)
+#' data <- data.frame(y = as.numeric(y), x = x)
+#' plot(x, y)
+#' mod_fitted <- setThreshold(y ~ bbs(x), data = data, mstop = 900)
+#' points(x, mod_fitted, col = "blue", pch = 17)
+#'
+#' @import mboost
+#' @export
+setThreshold <- function(formula, data, tau = 0.9, mstop = 1000, ...) {
 
-  mod <- mboost:mboost(y ~ bbs(x), data, family = QuantReg(tau = 0.9),
-                       baselearner = "bbs")
+  mod <- mboost::mboost(formula = formula, data = data,
+                       family = QuantReg(tau = tau),
+#                      baselearner = "bbs",
+                       control = boost_control(mstop = mstop, ...))
   return(fitted(mod))
-
 }
 
-# Q1: formula is an argument, so the user doesn't need to know to call y ~ bb(x) [the function bb],
-# because all of this is done by us behind the scenes so we implement it either way?
-#
-# Q2: how to connect data to y ~ bb(x), how do we extract the response and covariate, what is the form
-# of the object data that we provide?
-#
-# Q3: import quantreg beforehand, the whole package?
+
+
